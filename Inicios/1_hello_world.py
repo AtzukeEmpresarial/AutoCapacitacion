5928d7c0e49c2069da62ed89e7bb2db76263891c
Serposad <serposad@bancolombia.com.co>
2023-01-18 09:00:46 -0500

    Inicio de capacitación, conceptos basicos de python

---
+++ Inicios/1_hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+#esto es un comentario.
+print(type("Hello World"))
+
+variable_string = "oelo"
+variable_int = 12
+variable_booleana = True
+variable_flotante = 12.1
+variable_imag = 12 + 3j
+
+print(type(variable_imag))
---
+++ Inicios/2_operators.py
@@ -0,0 +1,33 @@
+#operadores como en todos los lenguajes
+print(3 + 1) #suma
+print(3 - 2) #resta
+print(4 / 2) #división , da siempre un flotante
+print(4 % 2) #modulo
+print(5 * 4) #multiplicación
+print(10 // 3) #división que aproxima al entero mas proximo, flordivision
+print(10 ** 3) #potenciación
+
+print("hola" + "mundo") #concatenación
+print("hola" + str(5)) #concatenación, el str convierte a str
+print("hola" * 4) #multiplicador de objetos, solo admite enteros (int)
+
+#OPERADORES COMPARATIVOS
+print(3 < 4)
+print(3 > 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+#comparadores de ordenación alfabetica, b>a como z>s pero s>b
+print("Hola" < "python")
+print("Hola" > "python")
+print("Hola" <= "python")
+print("Hola" >= "python")
+print("Hola" == "python")
+print("Hola" != "python")
+
+#operadores logicos son and y or, simple :v
+print(not(3<4))
+
+
