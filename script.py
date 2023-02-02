c291ba158c855280f763c599f78b9031e7ac4674
Serposad <serposad@bancolombia.com.co>
2023-02-02 11:13:59 -0500

    organizando el script
    Se reviza el script de Santiago y se organiza debido a que se transfirió mal, se usará de base para la conexión a las BD

--- script.py
+++ script.py
@@ -1,46 +1,76 @@
-# -*- coding: utf-8 -*-"""Created on Tues Mar 8 2022Modified on Mon May 9 2022@author: sanarbel"""from getpass import getpass, getuserfrom os import makedirs, pathimport pandas as pdimport pyodbcdef get_date():
-    """    Solicita la fecha de la conciliación por consola.    Returns    -------    date : String        Ubicación fisica del archivo seleccionado en el equipo que se ejecute.    """    print('><'*40)
+# -*- coding: utf-8 -*-"""Created on Tues Mar 8 2022Modified on Mon May 9 2022@author: sanarbel"""
+from getpass import getpass, getuser
+from os import makedirs, path 
+import pandas as pd
+import pyodbc
+def get_date():
+    """    Solicita la fecha de la conciliación por consola.   
+    Returns    -------    date : String        
+    Ubicación fisica del archivo seleccionado en el equipo que se ejecute.    """   
+    print('><'*40)
     date = input(
         'Escriba la fecha exacta que desea conciliar en formato AAAAMMDD: ')
     return int(date)
 def get_user():
-    """    Solicita el usuario de NACIONAL.    Returns    -------    usr : String        Ubicación fisica del archivo seleccionado en el equipo que se ejecute.    """    print('><'*40)
+    """    Solicita el usuario de NACIONAL.    
+    Returns    -------    usr : String        
+    Ubicación fisica del archivo seleccionado en el equipo que se ejecute.    """    
+    print('><'*40)
     print('Escriba su usuario de Nacional: ')
     usr = input()
-    return usrdef guardar(data, original_name):
-    """    Guarda el dataframe de datos encriptados en un archivo CSV    Parameters    ----------    data : DataFrame        Dataframe de datos encriptados    original_name : string        Nombre del archivo original    """    save_path = 'salidas/'    if not path.isdir(save_path):
+    return usr 
+def guardar(data, original_name):
+    """    Guarda el dataframe de datos encriptados en un archivo CSV    
+    Parameters    ----------    data : DataFrame        
+    Dataframe de datos encriptados    original_name : string }Nombre del archivo original  """    
+    save_path = 'salidas/'    
+    if not path.isdir(save_path):
         print(f'La ruta {save_path} no existe, se creará en esta carpeta...')
         makedirs(save_path)
     data.to_csv(
         save_path + original_name + '.csv',
         index=False,
     )
-    print('El archivo ha sido guardado en la ruta ' +          save_path + original_name + '.csv')
+    print('El archivo ha sido guardado en la ruta ' + save_path + original_name + '.csv')
 def inicio(opcion):
-    """    inicio ejecuta el proceso asociado a la opción seleccionada en el menú    Parameters    ----------    opcion : int        opción seleccionada del menú    Raises    ------    SystemExit        Cuando se marca salida, cierra el programa    """    INICIANDO_PROCESAMIENTO = 'Iniciando procesamiento...'    FINALIZACION_PRIMER_QUERY = 'Primer query terminado, comenzando la generación de 1 archivo...'    FINALIZACION_SEGUNDO_QUERY = 'Segundo query terminado, comenzando la generación de 1 archivo...'    FINALIZACION_TERCER_QUERY = 'Tercer query terminado, comenzando la generación de 1 archivo...'    opciones = [
+    """    inicio ejecuta el proceso asociado a la opción seleccionada en el menú  
+      Parameters    ----------    opcion : int        opción seleccionada del menú   
+       Raises    ------    SystemExit        
+       Cuando se marca salida, cierra el programa    """    
+    INICIANDO_PROCESAMIENTO = 'Iniciando procesamiento...'    
+    FINALIZACION_PRIMER_QUERY = 'Primer query terminado, comenzando la generación de 1 archivo...'    
+    FINALIZACION_SEGUNDO_QUERY = 'Segundo query terminado, comenzando la generación de 1 archivo...'    
+    FINALIZACION_TERCER_QUERY = 'Tercer query terminado, comenzando la generación de 1 archivo...'    
+    opciones = [
         '1',
         '2',
         '3',
         '4',
-        # '5', '6', '7'    ]
+        # '5', '6', '7'    
+        ]
     while opcion:
-        # Si la opcion es 8 entonces cierra el programa        if opcion == '8':
-            raise SystemExit        if opcion in opciones:
-            # Se solicita la fecha por consola            date = get_date()
-            # Se toma el usuario de sesión            user = 'N' + getuser()
-            # user = get_user()            # Pide la contraseña por consola            password = getpass(
-                f'Escriba su contraseña de Nacional para el usuario {user.upper()}: '            )
-            try:
-                cnx_nac = pyodbc.connect(
-                    'DSN=NACIONAL;UID=' + str(user) +                    ';PWD=' + str(password) + '',
-                    autocommit=True                )
-            except pyodbc.InterfaceError:
-                print('Usuario contraseña no valido...')
-                opcion = menu()
-                inicio(opcion)
+        # Si la opcion es 8 entonces cierra el programa        
+        if opcion == '8':
+            raise SystemExit        
+            if opcion in opciones:
+                # Se solicita la fecha por consola            
+                date = get_date()
+                # Se toma el usuario de sesión            
+                user = 'N' + getuser()
+                user = get_user()            
+                # Pide la contraseña por consola            
+                password = getpass(f'Escriba su contraseña de Nacional para el usuario {user.upper()}: ')
+                try:
+                    cnx_nac = pyodbc.connect(
+                        'DSN=NACIONAL;UID=' + str(user) + ';PWD=' + str(password) + '',
+                        autocommit=True )
+                except pyodbc.InterfaceError:
+                    print('Usuario contraseña no valido...')
+                    opcion = menu()
+                    inicio(opcion)
             if opcion == '1':
                 print(INICIANDO_PROCESAMIENTO)
-                sql_1 = f'''                    SELECT                        TMETYIDENT,                        TFUNCICODE,                        TPROCCCODI,                        TAPRVEDCOD,                        CONCAT(                            AMED1_004,                            RIGHT(TPRIACCNUM,4)                        ) AS TPRIACCNUM,                        TRANSAMOUN,                        TCADACEICO,                        TMESSREIND,                        TFECHAPROC,                        TLOCTRADAT,                        AMED1_001,                        AMED1_004,                        CONCAT(                            AMED1_004,                            RIGHT(AMED1_002,4)                        ), AS AMED1_002                        AMED1_048,                        AMNA1_001,                        AMNA1_002,                        AMNA1_017                       FROM                        TDCLIBRAMD.TDCFFET112                             INNER JOIN TDCLIBRAMD.TDCFFAMED1                             ON AMED1_001 = 807                             AND AMED1_002 = LPAD(TRIM(TPRIACCNUM),19, '0')                             LEFT JOIN TDCLIBRAMD.TDCFFAMNA1                             ON AMED1_001 = AMNA1_001                             AND AMED1_048 = AMNA1_002                     WHERE                        TSETTLINDI = 'M'                         AND TRECCURCOD = '170'                         AND TFUNCICODE <> '205'                         AND TFECHAPROC = '{date}'                '''                sql_2 = f'''                    SELECT                        ATPT1_001,                        ATPT1_002,                        CONCAT(                            ATPT1_002,                            RIGHT(ATPT1_026,4)                        ) AS ATPT1_026,                        ATPT1_011,                        ATPT1_007,                        ATPT1_006,                        ATPT1_013,                        ATPT1_009,                        ATPT1_014,                        ATPT1_030,                        ATPT1_018,                        ATPT1_025                     FROM                        TDCLIBRAMD.TDCFFATPT1                     WHERE                        ATPT1_009 IN (                            '2700','2701','2706','2734',                            '2735','2707','2704','2705',                            '2708','2709','471'                        )                         AND ATPT1_013 = '{date}'                         AND ATPT1_025 = 0                         AND ATPT1_003 = 0                 '''                sql_3 = f'''                    SELECT                        CTATJ_002 TARJETA,                        AMNA1_017 IDENTIFICACION,                        Y.*                     FROM                        TDCLIBRAMD.TDCFFCONTH Y                             LEFT JOIN TDCLIBRAMD.TDCFFCTATJ T                             ON CTATJ_004 = CONROCUENT                             LEFT JOIN TDCLIBRAMD.TDCFFAMBS2 A                             ON AMBS2_002 = CTATJ_004                             AND AMBS2_001 = 807                             LEFT JOIN TDCLIBRAMD.TDCFFAMNA1 AM                             ON AMBS2_005=AMNA1_002                             AND AMNA1_001=807                     WHERE                        cotipo = '01'                         AND covlrorigi <> '0'                         AND comoneda = '170'                         AND COCODIGOTR IN (                            '9090','9190','9091','9191',                            '9302','9202','9305','9205',                            '9303','9203','9204','9304'                        )                         AND COFECHAPOS = '{date}'                         AND LEFT(cologo,1) = '6'                         AND coorg = '807'                     ORDER BY                        AMNA1_017 DESC                '''                print('Ejecutando el primer query...')
+                sql_1 = f'''SELECT TMETYIDENT, TFUNCICODE, TPROCCCODI, TAPRVEDCOD,                        CONCAT(                            AMED1_004,                            RIGHT(TPRIACCNUM,4)                        ) AS TPRIACCNUM,                        TRANSAMOUN,                        TCADACEICO,                        TMESSREIND,                        TFECHAPROC,                        TLOCTRADAT,                        AMED1_001,                        AMED1_004,                        CONCAT(                            AMED1_004,                            RIGHT(AMED1_002,4)                        ), AS AMED1_002                        AMED1_048,                        AMNA1_001,                        AMNA1_002,                        AMNA1_017                       FROM                        TDCLIBRAMD.TDCFFET112                             INNER JOIN TDCLIBRAMD.TDCFFAMED1                             ON AMED1_001 = 807                             AND AMED1_002 = LPAD(TRIM(TPRIACCNUM),19, '0')                             LEFT JOIN TDCLIBRAMD.TDCFFAMNA1                             ON AMED1_001 = AMNA1_001                             AND AMED1_048 = AMNA1_002                     WHERE                        TSETTLINDI = 'M'                         AND TRECCURCOD = '170'                         AND TFUNCICODE <> '205'                         AND TFECHAPROC = '{date}'                '''                sql_2 = f'''                    SELECT                        ATPT1_001,                        ATPT1_002,                        CONCAT(                            ATPT1_002,                            RIGHT(ATPT1_026,4)                        ) AS ATPT1_026,                        ATPT1_011,                        ATPT1_007,                        ATPT1_006,                        ATPT1_013,                        ATPT1_009,                        ATPT1_014,                        ATPT1_030,                        ATPT1_018,                        ATPT1_025                     FROM                        TDCLIBRAMD.TDCFFATPT1                     WHERE                        ATPT1_009 IN (                            '2700','2701','2706','2734',                            '2735','2707','2704','2705',                            '2708','2709','471'                        )                         AND ATPT1_013 = '{date}'                         AND ATPT1_025 = 0                         AND ATPT1_003 = 0                 '''                sql_3 = f'''                    SELECT                        CTATJ_002 TARJETA,                        AMNA1_017 IDENTIFICACION,                        Y.*                     FROM                        TDCLIBRAMD.TDCFFCONTH Y                             LEFT JOIN TDCLIBRAMD.TDCFFCTATJ T                             ON CTATJ_004 = CONROCUENT                             LEFT JOIN TDCLIBRAMD.TDCFFAMBS2 A                             ON AMBS2_002 = CTATJ_004                             AND AMBS2_001 = 807                             LEFT JOIN TDCLIBRAMD.TDCFFAMNA1 AM                             ON AMBS2_005=AMNA1_002                             AND AMNA1_001=807                     WHERE                        cotipo = '01'                         AND covlrorigi <> '0'                         AND comoneda = '170'                         AND COCODIGOTR IN (                            '9090','9190','9091','9191',                            '9302','9202','9305','9205',                            '9303','9203','9204','9304'                        )                         AND COFECHAPOS = '{date}'                         AND LEFT(cologo,1) = '6'                         AND coorg = '807'                     ORDER BY                        AMNA1_017 DESC                '''                print('Ejecutando el primer query...')
                 df_1 = pd.read_sql(sql_1, cnx_nac)
                 print('Primer query terminado, comenzando la generación de 3 archivos')
                 guardar(
