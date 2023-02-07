df56d7d0a4cbf9244cfb0c28903f36c69bfa2d4e
Atzuke <122810555+AtzukeEmpresarial@users.noreply.github.com>
2023-02-06 20:01:55 -0500

    Side bar buttons funcionales
    Se modifica la forma en que funciona el cambio de los botones en el sidebar, ahora entran sus estilos desde la funcion que los invoca.

--- Inventarios/Screens/side_bar.py
+++ Inventarios/Screens/side_bar.py
@@ -13,20 +13,27 @@ class side_bar(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         #self.configure(fg_color = style.BACKGROUND)
-        side_bar_color = style.SIDE_BAR_COLOR
+        button1_color = style.BUTTONSTYLE
+        button2_color = style.BUTTONSTYLE
         self.controller = controller
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(4,weight=1)
-        self.init_widgets(side_bar_color)
+        self.grid_rowconfigure(0,weight=1)
+        self.grid_rowconfigure(1,weight=3)
+        self.grid_rowconfigure(2,weight=3)
+        self.grid_rowconfigure(3,weight=10)
+        self.configure(fg_color= style.GRAY)
+        self.init_widgets(button1_color, button2_color)
     
     def set_login_frame(self):
             self.controller.show_frame(menu_login)
-            style.SIDE_BAR_COLOR = "#FFFFFF"
+            self.init_widgets(style.ALTER_BUTTONSTYLE,style.BUTTONSTYLE)
+            
 
     def set_process_frame(self):
             self.controller.show_frame(menu_process)
+            self.init_widgets(style.BUTTONSTYLE,style.ALTER_BUTTONSTYLE)
 
-    def init_widgets(self, side_bar_color):
+    def init_widgets(self, button1_style,button2_style):
         lb_tittle = ctk.CTkLabel(
             self,
             text = "INVENTARIOS",
@@ -35,37 +42,58 @@ class side_bar(ctk.CTkFrame):
         )
         lb_tittle.grid(
             row = 0, 
-            padx = 20,
-            pady =(20,10)
+            padx = 10,
+            pady =10
             )
-        flogin_option = ctk.CTkFrame(self, corner_radius=0)
-        flogin_option.grid(row = 2, column = 0, sticky = ctk.NSEW)
+        
+        flogin_option = ctk.CTkFrame(
+              self, 
+              corner_radius=0
+              )
+        flogin_option.grid(
+              row = 1,
+              column = 0, 
+              sticky = ctk.NSEW
+              )
         bt_login = ctk.CTkButton(
             flogin_option,
             text = "LOG IN",
-            **style.STYLELABELTITLES,
-            fg_color = side_bar_color,
-            hover =True,
+            **button1_style,
             command= self.set_login_frame,
+            corner_radius=0
         )
         bt_login.grid(
             row = 0, 
-            padx = 20,
-            pady = 20
+            padx = 10,
+            pady = 10
         )
-        bt_login.pack(fill = ctk.BOTH, expand = True)
-        fprocesar_option = ctk.CTkFrame(self, corner_radius=0)
-        fprocesar_option.grid(row = 3, column = 0, sticky = ctk.NSEW)
+        bt_login.pack(
+              fill = ctk.BOTH, 
+              expand = True
+              )
+        
+        fprocesar_option = ctk.CTkFrame(
+              self, 
+              corner_radius=0
+              )
+        fprocesar_option.grid(
+              row = 2, 
+              column = 0, 
+              sticky = ctk.NSEW
+              )
         bt_procesar = ctk.CTkButton(
             fprocesar_option,
             text = "PROCESAR",
-            **style.STYLELABELTITLES,
-            hover =True,
-            command= self.set_process_frame
-        )
+            **button2_style,
+            command= self.set_process_frame,
+            corner_radius=0
+            )
         bt_procesar.grid(
             row = 0, 
-            padx = 20,
-            pady = 20
-        ) 
-        bt_procesar.pack(fill = ctk.BOTH, expand = True)   
+            padx = 10,
+            pady = 10
+            ) 
+        bt_procesar.pack(
+              fill = ctk.BOTH, 
+              expand = True
+              )   
--- Inventarios/constants/style.py
+++ Inventarios/constants/style.py
@@ -1,8 +1,15 @@
-BACKGROUND = "#5B8FB9"
-FONT = ("Roboto", 16)
-FONTTITLES = ("Roboto", 24, "bold")
-COMPONENT = "#5B8FB9"
-TEXT = "#FFFFFF"
+GRAY = "#313131"
+WHITE = "#FFFFFF"
+BLACK = "#000000"
+SOMEBLUE = "#5B8FB9"
+GRAYWHITE = "#A0A0A0"
+
+BACKGROUND = SOMEBLUE
+FONT = ("Calibri Bold", 16)
+FONTTITLES = ("Calibri Bold", 38, "bold")
+FONTBUTTON = ("Calibri Bold", 30, "bold")
+COMPONENT = SOMEBLUE
+TEXT = WHITE
 
 STYLELABEL = {
     "font" : FONT,
@@ -13,4 +20,17 @@ STYLELABELTITLES = {
     "text_color": TEXT
 }
 
-SIDE_BAR_COLOR = "#000000"
+BUTTONSTYLE = {
+    "font" : FONTBUTTON,
+    "fg_color" : GRAY,
+    "text_color" : TEXT,
+    "hover_color" : GRAYWHITE,
+    "hover" : True
+}
+ALTER_BUTTONSTYLE = {
+    "font" : FONTBUTTON,
+    "fg_color" : WHITE,
+    "text_color" : BLACK,
+    "hover_color" : GRAYWHITE,
+    "hover" : True
+}
